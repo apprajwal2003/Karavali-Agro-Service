fe866526e83f08667ba645557de7040dbdd9a108
A P Prajwal <apprajwalwork@gmail.com>
2025-06-12 22:41:07 +0530

    cart icon

--- src/components/Collections Components/CollectionsNavbar.tsx
+++ src/components/Collections Components/CollectionsNavbar.tsx
@@ -69,6 +69,19 @@ export default function CollectionsNavbar() {
           className="hidden md:block p-2 rounded border text-white"
         />
 
+        <div className="relative inline-block">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32 32"
+            className="w-8 h-8"
+          >
+            <path d="M29.4 8.85A2.48 2.48 0 0 0 27.53 8H14a1 1 0 0 0 0 2h13.53a.47.47 0 0 1 .36.16.48.48 0 0 1 .11.36l-1.45 10a1.71 1.71 0 0 1-1.7 1.48H14.23a1.72 1.72 0 0 1-1.68-1.33L10 8.79l-.5-1.92A3.79 3.79 0 0 0 5.82 4H3a1 1 0 0 0 0 2h2.82a1.8 1.8 0 0 1 1.74 1.36L8 9.21l2.6 11.88A3.72 3.72 0 0 0 14.23 24h10.62a3.74 3.74 0 0 0 3.68-3.16l1.45-10a2.45 2.45 0 0 0-.58-1.99zM16 25h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM25 25h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2z" />
+          </svg>
+          <span className="absolute top-0 right-0 inline-flex items-center justify-center px-1.5 py-0.5 text-xs font-bold leading-none text-white bg-red-600 rounded-full">
+            {1}
+          </span>
+        </div>
+
         <Image
           src="/logo.png"
           width={40}
