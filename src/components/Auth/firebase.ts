9a4db6c7cb62a4eba903c057a28c65b622fdaf4a
A P Prajwal <apprajwalwork@gmail.com>
2025-07-06 23:09:00 +0530

    gg

--- src/components/Auth/firebase.ts
+++ src/components/Auth/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAe6fxN3v-XOENfDpLdHYjyeumjoeGkUI",
-  authDomain: "karavali-agro-services.firebaseapp.com",
-  projectId: "karavali-agro-services",
-  storageBucket: "karavali-agro-services.appshot.app",
-  messagingSenderId: "605480877382",
-  appId: "1:605480877382:web:6bbf15a861b50606c6b947",
-  measurementId: "G-0Z8WP6913J",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
