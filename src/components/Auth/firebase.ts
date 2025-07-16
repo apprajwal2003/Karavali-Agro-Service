c47e0da2a8b1165db32ed4f49a82cb15f8ab17b3
A P Prajwal <apprajwalwork@gmail.com>
2025-07-16 11:03:15 +0530

    auth

--- src/components/Auth/firebase.ts
+++ src/components/Auth/firebase.ts
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -15,7 +16,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = getApps().length == 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
